6d70133ef17ef3123cf521c7e3c06ff572f850aa
Bouchra Denna <bouchradenna@gmail.com>
2023-05-24 01:37:13 +0100

    maj

--- models/departenemt.model.js
+++ models/departenemt.model.js
@@ -19,7 +19,6 @@ module.exports = {
     getAll,
     getOne,
     deleteOne,
-    setReference,
     setlabel,
     setDesc,
 }
@@ -29,28 +28,99 @@ function addNew(label,desc) {
     
 }
 
-//Get all
-function getAll() {
-    
-}
+// Récupération de tous les départements
+  function getAll() {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(dbUrl, { useNewUrlParser: true })
+        .then(() => {
+          return Departement.find();
+        })
+        .then(departements => {
+          mongoose.disconnect();
+          resolve(departements);
+        })
+        .catch(error => {
+          mongoose.disconnect();
+          reject(error);
+        });
+    });
+  }
+  
 
-//Get one
-function getOne(ref) {
-    
-}
-
-//delete one
-function deleteOne(ref) {
-    
-}
-
-function setReference(departement, ref) {
-    
-}
-
-function setlabel(departement, label) {
-    
-}
-function setDesc(departement, desc) {
-    
-}
+  // Récupération d'un département par référence
+  function getOne(ref) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(dbUrl, { useNewUrlParser: true })
+        .then(() => {
+          return Departement.findOne({ reference: ref });
+        })
+        .then(departement => {
+          mongoose.disconnect();
+          resolve(departement);
+        })
+        .catch(error => {
+          mongoose.disconnect();
+          reject(error);
+        });
+    });
+  }
+  
+  // Suppression d'un département par référence
+  function deleteOne(ref) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(dbUrl, { useNewUrlParser: true })
+        .then(() => {
+          return Departement.deleteOne({ reference: ref });
+        })
+        .then(result => {
+          mongoose.disconnect();
+          if (result.deletedCount === 1) {
+            resolve("Document supprimé avec succès.");
+          } else {
+            resolve(`Aucun document trouvé avec la référence : ${ref}`);
+          }
+        })
+        .catch(error => {
+          mongoose.disconnect();
+          reject(error);
+        });
+    });
+  }
+  
+  // Mise à jour de la description d'un département
+  function setDesc(ref, desc) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(dbUrl, { useNewUrlParser: true })
+        .then(() => {
+          return Departement.updateOne({ reference: ref }, { description: desc });
+        })
+        .then(() => {
+          mongoose.disconnect();
+          resolve("Référence mise à jour avec succès.");
+        })
+        .catch(error => {
+          mongoose.disconnect();
+          reject(error);
+        });
+    });
+  }
+  
+  // Mise à jour du libellé d'un département
+  function setlabel(ref, label) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(dbUrl, { useNewUrlParser: true })
+        .then(() => {
+          return Departement.updateOne({ reference: ref }, { label: label });
+        })
+        .then(() => {
+          mongoose.disconnect();
+          resolve("Libellé mis à jour avec succès.");
+        })
+        .catch(error => {
+          mongoose.disconnect();
+          reject(error);
+        });
+    });
+  }
+  
+  
