1428494ae4eea5407de6b6ee835a95553944ea61
mustapha el mtarkati <mustapha_elmtarkati@um5.ac.ma>
2023-05-25 00:06:56 +0100

    25/05

--- globals.js
+++ globals.js
@@ -1,7 +1,8 @@
 const crypto = require("crypto")
 const key = "blockChainForAll";
 const iv = crypto.randomBytes(16);;
-global.urlDb = "mongodb+srv://mstafamt8:mstafa123@cluster0.7ltwxxt.mongodb.net/?retryWrites=true&w=majority";
+// global.urlDb = "mongodb+srv://mstafamt8:mstafa123@cluster0.7ltwxxt.mongodb.net/?retryWrites=true&w=majority";
+global.urlDb = "mongodb://127.0.0.1:27017/test";
 global.encrypt= encrypt;
 global.decrypt= decrypt;
 
