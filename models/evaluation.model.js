8adf4fdee988d0102d6e6ba27eedd405c7cb1a33
Bouchra Denna <bouchradenna@gmail.com>
2023-05-25 17:31:31 +0100

    e

--- models/evaluation.model.js
+++ models/evaluation.model.js
@@ -22,8 +22,6 @@ module.exports = {
   getAll,
   getOne,
   deleteOne,
-  referenceModule,
-  referenceStudent,
   setDate,
   setGrade,
 };
@@ -58,16 +56,130 @@ function addNew(refModule, refStudent, grade, date) {
 }
 
 //Get all
-function getAll() {}
+function getAll() {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(dbUrl, { useNewUrlParser: true })
+      .then(() => {
+        Evaluation.find()
+          .then((evaluations) => {
+            mongoose.disconnect();
+            resolve(evaluations);
+          })
+          .catch((err) => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
 
 //Get one
-function getOne(ref) {}
+function getOne(refStudent, refModule) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(dbUrl, { useNewUrlParser: true })
+      .then(() => {
+        Evaluation.findOne({
+          referenceModule: refModule,
+          referenceStudent: refStudent,
+        })
+          .then((evaluation) => {
+            mongoose.disconnect();
+            resolve(evaluation);
+          })
+          .catch((err) => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
 
 //delete one
-function deleteOne(ref) {}
+function deleteOne(refStudent, refModule) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(dbUrl, { useNewUrlParser: true })
+      .then(() => {
+        Evaluation.deleteOne({
+          referenceModule: refModule,
+          referenceStudent: refStudent,
+        })
+          .then((result) => {
+            mongoose.disconnect();
+            if (result.deletedCount === 1) {
+              resolve("Evaluation supprimée avec succès.");
+            } else {
+              resolve(
+                "Aucune évaluation trouvée avec les références fournies."
+              );
+            }
+          })
+          .catch((err) => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
 
-function referenceModule(refStudent, refModule, newRef) {}
+function setDate(refStudent, refModule, date) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(dbUrl, { useNewUrlParser: true })
+      .then(() => {
+        Evaluation.findOneAndUpdate(
+          { referenceModule: refModule, referenceStudent: refStudent },
+          { date: date }
+        )
+          .then(() => {
+            mongoose.disconnect();
+            resolve("Date de l'évaluation mise à jour avec succès.");
+          })
+          .catch((err) => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
 
-function referenceStudent(refStudent, refModule, newRef) {}
-function setDate(refStudent, refModule, date) {}
-function setGrade(refStudent, refModule, grade) {}
+function setGrade(refStudent, refModule, grade) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(dbUrl, { useNewUrlParser: true })
+      .then(() => {
+        Evaluation.findOneAndUpdate(
+          { referenceModule: refModule, referenceStudent: refStudent },
+          { grade: grade }
+        )
+          .then(() => {
+            mongoose.disconnect();
+            resolve("Note de l'évaluation mise à jour avec succès.");
+          })
+          .catch((err) => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+
+
